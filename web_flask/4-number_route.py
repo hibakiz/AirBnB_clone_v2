26e9d2bc0f7db8f20b894b892ed9a54e814f9c52
hibakiz <hbosalah98sd@gmail.com>
2024-03-23 00:27:16 +0300

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""run the flask """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return ("Hello HBNB!")
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return ("HBNB")
+
+@app.route("/c/<string>", strict_slashes=False)
+def c_is(string):
+    if string:
+        new = string.replace("_", " ")
+        return (f"C {new}")
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<string>", strict_slashes=False)
+def Python_is(string="is cool"):
+    new = string.replace("_", " ")
+    return (f"Python {new}")
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    try:
+        n = int(n)
+        return (f"{n} is a number")
+    except ValueError:
+        return
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
