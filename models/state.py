1a3f9de42451ed8f6304e79620a25debf8627d7a
hibakiz <hbosalah98sd@gmail.com>
2024-03-24 23:31:18 +0300

    Base

--- models/state.py
+++ models/state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" class State"""
+""" holds class State"""
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
@@ -10,7 +10,7 @@ from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """state class"""
+    """Representation of state """
     if models.storage_t == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
@@ -19,13 +19,13 @@ class State(BaseModel, Base):
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes"""
+        """initializes state"""
         super().__init__(*args, **kwargs)
 
     if models.storage_t != "db":
         @property
         def cities(self):
-            """getter for state"""
+            """getter for list of city instances related to the state"""
             city_list = []
             all_cities = models.storage.all(City)
             for city in all_cities.values():
